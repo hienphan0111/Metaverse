9586eea79398b71c78acf6727a4a1d049198bc97
hienphan0111 <hienphan@digitalcad.edu.vn>
2023-04-22 16:53:43 +0700

    Add getstarted feature

--- sections/Explore.jsx
+++ sections/Explore.jsx
@@ -9,7 +9,9 @@ import { staggerContainer } from '../utils/motion'
 import { ExploreCard, TitleText, TypingText } from '../components';
 import { exploreWorlds } from '../constants'
 
-const Explore = () => (
+const Explore = () => {
+  const [active, setActive] = useState('world-2');
+  return (
   <section className={`${styles.paddings}`} id="explore">
     <motion.div
       variants={staggerContainer}
@@ -18,13 +20,22 @@ const Explore = () => (
       viewport={{once: false, amount:0.25}}
       className={`${styles.innerWidth} mx-auto flex flex-col`}
     >
-      <TypingText title="| The World" textStyles="" />
-      <TitleText title={<>Choose the world you want
-        <br className="md:block hidden" /> to explore
-      </>} textStyles="text-center" 
+      <TypingText title="| The World" textStyles="text-center" />
+      <TitleText title={<>Choose the world you want <br  className="md:block hidden" /> to explore </>} textStyles="text-center" 
       />
+      <div className="mt-[50px] flex lg:flex-row flex-col min-h-[70vh] gap-5">
+        {exploreWorlds.map((world, index) => (
+          <ExploreCard
+            key={world.id}
+            {...world}
+            index={index}
+            active={active}
+            handleClick={setActive}
+          />
+        ))}
+      </div>
     </motion.div>
   </section>
-);
+)};
 
 export default Explore;
--- sections/World.jsx
+++ sections/World.jsx
@@ -1,8 +1,53 @@
 'use client';
 
+import { motion } from 'framer-motion';
+
+import styles from '../styles';
+
+import { staggerContainer, fadeIn, planetVariants } from '../utils/motion'
+import { TitleText, TypingText } from '../components';
+
 const World = () => (
-  <section>
-    World section
+  <section className={`${styles.paddings} relative z-10`}>
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{once: 'false', amount: 0.25}}
+      className={`${styles.innerWidth} mx-auto flex flex-col`}
+    >
+      <TypingText title="| People on the world" textStyles="text-center" />
+      <TitleText title={(
+        <>
+          Track friends around you and invite them to play together in the same world
+        </>
+      )} textStyles="text-center" />
+
+      <motion.div
+        variants={fadeIn('up', 'tween', 0.3, 1)}
+        className="realative mt-[68px] flex w-full h0 [550px]"
+      >
+        <img src="/map.png" alt="map" 
+          className="w-full h-full object-cover"
+        />
+
+        <div className="absolute bottom-20 right-20 w-[70px] h-[70px] p-[6px] rounded-full bg-[#5d6680]">
+          <img src="people-01.png" alt="people"
+            className="w-full h-full"
+          />
+        </div>
+        <div className="absolute top-50 left-20 w-[70px] h-[70px] p-[6px] rounded-full bg-[#5d6680]">
+          <img src="people-02.png" alt="people"
+            className="w-full h-full"
+          />
+        </div>
+        <div className="absolute bottom-30 left-[45%] w-[70px] h-[70px] p-[6px] rounded-full bg-[#5d6680]">
+          <img src="people-03.png" alt="people"
+            className="w-full h-full"
+          />
+        </div>
+      </motion.div>
+    </motion.div>
   </section>
 );
 
