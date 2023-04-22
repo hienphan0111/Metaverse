dc469a59aa27f4272112f2b6356fc80158c4acb7
Hienphan0111 <hienphan@digitalcad.edu.vn>
2023-04-22 11:53:48 +0700

    Add about section

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -12,7 +12,6 @@ const Navbar = () => (
     whileInView="show"
     className={`${styles.xPaddings} py-8 relative`}
   >
-    navbar
     <div className="absolute w-[50%] inset-0 gradient-01" />
     <div className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}>
       <img 
